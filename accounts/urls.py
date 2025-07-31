47a610d0519ed842bc8eb7643db4892824030dde
BtJ <ymawro@gmail.com>
2025-07-31 15:41:03 +0900

    feat: add home template for testing accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
+from django.views.generic import TemplateView
 from django.urls import path
 from .views import SignUpView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
