4341f23ca3e8a3fbe776503ba597332ebcbfce47
BtJ <ymawro@gmail.com>
2025-07-31 16:01:06 +0900

    feat: add register CustomUser to admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import CustomUser
+
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
+    search_fields = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+    )
