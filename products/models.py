5f13d5fb532536bfcec4530657431b80a622fefb
BtJ <ymawro@gmail.com>
2025-08-03 14:41:53 +0900

    feat: add Author model

--- products/models.py
+++ products/models.py
@@ -4,3 +4,12 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name}({self.birth_date.strftime('%Y/%m/%d')})"
