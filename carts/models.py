2aea7eb6d5583508d1f82ae39b6e6a8ef8fb4b84
BtJ <ymawro@gmail.com>
2025-08-08 15:57:13 +0900

    feat: add Cart model

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from django.db import models
+from django.core.validators import MinValueValidator
+from django.conf import settings
+
+from products.models import Book
+
+
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField(validators=[MinValueValidator(1)])
+
+    class Meta:
+        unique_together = ["user", "product"]
+
+    def get_total_price(self):
+        return self.product.price * self.quantity
