927fde1484715b483c7ec018b4aa3a1dd735de72
BtJ <ymawro@gmail.com>
2025-08-08 14:53:38 +0900

    feat: add admin register for Author, Book and Category

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Author, Book, Category
+
+
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["name", "birth_date"]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["name", "author", "price", "stock", "publication_date", "category"]
+    list_editable = ["price", "stock"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
