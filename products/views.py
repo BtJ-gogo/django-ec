297ce30a09c561436937355441a9460042b1d16f
BtJ <ymawro@gmail.com>
2025-08-08 15:10:51 +0900

    feat: add BooklistView

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
+from django.views.generic import ListView,
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
+    paginate_by = 20
