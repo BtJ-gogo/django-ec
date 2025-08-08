623e49d1f98534b8d17d84ce21ad015a892c6d84
BtJ <ymawro@gmail.com>
2025-08-08 15:20:29 +0900

    feat: add urlpatterns for AuthorDetail, BookList and BookDetail

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from .views import AuthorDetailView, BookListView, BookDetailView
+
 app_name = "products"
 
 urlpatterns = [
+    path("author/<int:pk>/", AuthorDetailView.as_view(), name="author_detail"),
+    path("books/<int:pk>/", BookDetailView.as_view(), name="book_detail"),
+    path("books/", BookListView.as_view(), name="book_list"),
     path(
-        "", TemplateView.as_view(template_name="product_home.html"), name="product_home"
-    )
+        "",
+        TemplateView.as_view(template_name="product_home.html"),
+        name="product_home",
+    ),
 ]
