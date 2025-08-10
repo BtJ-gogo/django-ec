7eb98608b2c59ea18073d261b2278f5620a91c07
BtJ <ymawro@gmail.com>
2025-08-10 19:02:40 +0900

    feat: add shipping address feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import MypageView, ShippingAddressView
+from .views import MypageView, ShippingAddressView, ShippingAddressAddView
 
 app_name = "accounts"
 
 urlpatterns = [
+    path("shipping/add/", ShippingAddressAddView.as_view(), name="shipping_add"),
     path("shipping/", ShippingAddressView.as_view(), name="shipping"),
     path("", MypageView.as_view(), name="mypage"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import ListView, TemplateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import ShippingAddress
+from .forms import ShippingAddressForm
 
 
 class MypageView(TemplateView):
@@ -10,3 +13,14 @@ class MypageView(TemplateView):
 class ShippingAddressView(ListView):
     model = ShippingAddress
     template_name = "shipping_address_list.html"
+
+
+class ShippingAddressAddView(CreateView):
+    model = ShippingAddress
+    template_name = "shipping_address_add.html"
+    form_class = ShippingAddressForm
+    success_url = reverse_lazy("accounts:shipping")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
