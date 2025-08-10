498b6fc5d0791570728d80ed67406811af4c9ed9
BtJ <ymawro@gmail.com>
2025-08-10 18:02:48 +0900

    feat add ShippngAddressView

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+
+from .models import ShippingAddress
 
 
 class MypageView(TemplateView):
     template_name = "mypage.html"
+
+
+class ShippingAddressView(ListView):
+    model = ShippingAddress
+    template_name = "shipping_address_list.html"
