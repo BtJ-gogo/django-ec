44eed97b91387d7a0565fb77a54c2e3e000c3082
BtJ <ymawro@gmail.com>
2025-08-10 22:01:58 +0900

    feat: add LoginRequiredMixin to all views

--- accounts/views.py
+++ accounts/views.py
@@ -2,21 +2,22 @@ from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView, TemplateView, View
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import ShippingAddress
 from .forms import ShippingAddressForm
 
 
-class MypageView(TemplateView):
+class MypageView(LoginRequiredMixin, TemplateView):
     template_name = "mypage.html"
 
 
-class ShippingAddressView(ListView):
+class ShippingAddressView(LoginRequiredMixin, ListView):
     model = ShippingAddress
     template_name = "shipping_address_list.html"
 
 
-class ShippingAddressAddView(CreateView):
+class ShippingAddressAddView(LoginRequiredMixin, CreateView):
     model = ShippingAddress
     template_name = "shipping_address_add.html"
     form_class = ShippingAddressForm
@@ -27,14 +28,14 @@ class ShippingAddressAddView(CreateView):
         return super().form_valid(form)
 
 
-class ShippingAddressUpdateView(UpdateView):
+class ShippingAddressUpdateView(LoginRequiredMixin, UpdateView):
     model = ShippingAddress
     template_name = "shipping_address_update.html"
     form_class = ShippingAddressForm
     success_url = reverse_lazy("accounts:shipping")
 
 
-class ShippingAddressDeleteView(View):
+class ShippingAddressDeleteView(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         obj = get_object_or_404(
             ShippingAddress, user=self.request.user, id=kwargs.get("pk")
