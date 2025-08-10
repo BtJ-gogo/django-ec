e2f7969cb59e0cd98f53d93d816db58d8a0af1bc
BtJ <ymawro@gmail.com>
2025-08-10 19:03:34 +0900

    feat: chage import to accounts.form

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import FormMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from carts.models import Cart
 
-# Create your views here.
+from accounts.forms import ShippingAddressForm
+
+
+class OrderView(LoginRequiredMixin, FormMixin, ListView):
+    model = Cart
+    template_name = "order.html"
+    form_class = ShippingAddressForm
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user).prefetch_related()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        total = 0
+        for row in self.object_list:
+            total += row.get_total_price()
+        context["total"] = total
+        return context
