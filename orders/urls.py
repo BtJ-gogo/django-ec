adfe06c07c90319a3dac01d6f6a0b5463915230a
BtJ <ymawro@gmail.com>
2025-08-12 19:38:04 +0900

    feat: add path for OrderCompletedView and OrderCancelView

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import OrderView, AddressSelectView
+from .views import OrderView, AddressSelectView, OrderCanceledView, OrderCompletedView
 
 app_name = "orders"
 
 urlpatterns = [
+    path("completed/", OrderCompletedView.as_view(), name="order_completed"),
+    path("canceled/", OrderCanceledView.as_view(), name="order_canceled"),
     path("address/", AddressSelectView.as_view(), name="address_select"),
     path("", OrderView.as_view(), name="order"),
 ]
