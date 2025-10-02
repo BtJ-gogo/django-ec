6b6a9748d8fa4897403dbe878ce79ca8a97e9486
BtJ <ymawro@gmail.com>
2025-10-02 11:51:55 +0900

    test: add test code for HomeView

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomeViewTest(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("pages:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("pages:home"))
+        self.assertTemplateUsed(response, "home.html")
