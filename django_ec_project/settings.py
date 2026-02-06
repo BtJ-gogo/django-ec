1ff22c931845214b45ed9e1351a1a278d56f39d0
BtJ <ymawro@gmail.com>
2026-02-06 16:02:01 +0900

    feat: change settings name

--- django_ec_project/settings.py
+++ django_ec_project/settings.py
@@ -107,14 +107,9 @@ WSGI_APPLICATION = "django_ec_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
-        # local hostではなくDockerのサービス名を指定
-        'HOST': 'db',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
